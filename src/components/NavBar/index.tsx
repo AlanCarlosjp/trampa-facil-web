e295918411961b93c2e11403b443f3455ad2a60b
AlanCarlosjp <alandepaulajr@gmail.com>
2023-01-06 21:33:51 -0300

    base

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.scss';
+
+
+const NavBar = () => {
+    return (
+        <div>
+            NavBar
+        </div>
+    )
+}
+
+
+export default NavBar;
---
+++ src/pages/Routes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Home from "./Home";
+
+
+const Routes = () => {
+    return(
+        <>
+        <NavBar />
+        <Home />
+        </>
+    )
+}
+
+
+export default Routes;
